2884e6be3114e84b56000c6d446b2b975a5eb890
fraolb <fraolbereket@gmail.com>
2022-04-11 20:43:51 -0900

    data files of the  gallery

---
+++ src/components/data.js
@@ -0,0 +1,50 @@
+
+
+import swim from "../images/swim.png";
+import mood from "../images/mood.png";
+import fitness from "../images/fitness.png";
+
+export default[
+    {
+        image:"https://picsum.photos/id/1001/5616/3744",
+        rating:"5",
+        number:"6",
+        country:"kenya",
+        story:"swimming lessons with fraol",
+    },
+    {
+        image:"https://picsum.photos/id/1025/4951/3301",
+        rating:"4.5",
+        number:"4",
+        country:"uganda",
+        story:"photo shoot with fraol",
+    },
+    {
+        image:"https://picsum.photos/id/1024/1920/1280",
+        rating:"3",
+        number:"3",
+        country:"Ethiopia",
+        story:"fitness training with fraol",
+    },
+    {
+        image:"https://picsum.photos/id/1010/5184/3456",
+        rating:"5",
+        number:"6",
+        country:"kenya",
+        story:"swimming lessons with fraol",
+    },
+    {
+        image:"https://picsum.photos/id/1005/5760/3840",
+        rating:"4.5",
+        number:"4",
+        country:"uganda",
+        story:"photo shoot with fraol",
+    },
+    {
+        image:"https://picsum.photos/id/1002/4312/2868",
+        rating:"3",
+        number:"3",
+        country:"Ethiopia",
+        story:"fitness training with fraol",
+    }
+]
